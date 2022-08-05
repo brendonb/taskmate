61b151aaa19cf850e61e1b5846cdb5af22873682
brendonb <brendonb@users.noreply.github.com>
2022-08-05 19:21:47 +0200

    remove spaces secretkey

--- taskman/settings.py
+++ taskman/settings.py
@@ -15,12 +15,12 @@ environ.Env.read_env(os.path.join(BASE_DIR,'.env'))
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('DJANGO_SECRET_KEY')
+SECRET_KEY=env('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env('DJANGO_DEBUG')
+DEBUG=env('DJANGO_DEBUG')
 
-ALLOWED_HOSTS = [env('DJANGO_ALLOWED_HOST')]
+ALLOWED_HOSTS=[env('DJANGO_ALLOWED_HOST')]
 
 
 # Application definition
